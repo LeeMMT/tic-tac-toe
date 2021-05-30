2ab1d61e849bbb26c94b33ff6f4634c147fa39d2
Marcus <marcuslt95@gmail.com>
2021-05-31 00:38:05 +0100

    Added function to render contents of gridCells array to the board

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 let player1;
 let player2;
 
-const gameFlow = function() {
+const gameFlow = (function() {
     
-}
+})();
 
 const gameBoard = (function() {
-    let topRow = ['', '', ''];
-    let middleRow = ['', '', ''];
-    let bottomRow = ['', '', ''];
+    let gridCells = ['O', '', '', '', 'X', '', '', '', ''];
+    const updateBoard = function() {
+        gridCells.forEach(element => {
+        document.querySelector(`div[data-cell="${gridCells.indexOf(element)}"]`).textContent = element;
+    })
+    }
+    return {
+        updateBoard
+    };
 })();
 
 const player = function(name, marker) {
