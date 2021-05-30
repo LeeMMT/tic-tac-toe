6e4e57dd5a4842880595cb412dc06c8458b1e8a3
Marcus <marcuslt95@gmail.com>
2021-05-30 17:34:44 +0100

    Created the foundation for a couple of core modules, uncluding the game board and the object which will control flow of the game. Created player facory function. Began to play around with basic ideas for general aesthetic in html and css

---
+++ script.js
@@ -0,0 +1,16 @@
+let player1;
+let player2;
+
+const gameFlow = function() {
+    
+}
+
+const gameBoard = (function() {
+    let topRow = ['', '', ''];
+    let middleRow = ['', '', ''];
+    let bottomRow = ['', '', ''];
+})();
+
+const player = function(name, marker) {
+    return {name, marker}
+}
